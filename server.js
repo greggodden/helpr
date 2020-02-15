ab0b6a9b80997fc302f897010389174bc446daea
greggodden <email@greggodden.com>
2020-02-15 15:25:30 -0500

    helpr setup

---
+++ server.js
@@ -0,0 +1,19 @@
+let express = require('express')
+let app = express()
+let reloadMagic = require('./reload-magic.js')
+
+reloadMagic(app)
+
+app.use('/', express.static('build')); // Needed for the HTML and JS files
+app.use('/', express.static('public')); // Needed for local assets
+
+// Your endpoints go after this line
+
+// Your endpoints go before this line
+
+app.all('/*', (req, res, next) => { // needed for react router
+    res.sendFile(__dirname + '/build/index.html');
+})
+
+
+app.listen(4000, '0.0.0.0', () => { console.log("Server running on port 4000") })
